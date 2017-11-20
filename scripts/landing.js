7b1b6c917da06c2f63bb578ba2bba4e8bd8694a3
StevenNC <steven.ooey@gmail.com>
2017-11-20 10:08:27 -0800

    Assignment 23 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,14 @@
 var pointsArray = document.getElementsByClassName('point');
 
-  var animatePoints = function(points) {
+var revealPoint = function(point) {
+      point.style.opacity = 1;
+      point.style.transform = "scaleX(1) translateY(0)";
+      point.style.msTransform = "scaleX(1) translateY(0)";
+      point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
 
-    var revealPoint = function(i) {
-        for (var i=0; i < points.length; i++) {
-          points[i].style.opacity = 1;
-          points[i].style.transform = "scaleX(1) translateY(0)";
-          points[i].style.msTransform = "scaleX(1) translateY(0)";
-          points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-        }
-    };
-    revealPoint();
-
+var animatePoints = function(points) {
+    forEach(points, revealPoint);
 };
 
 window.onload = function() {
@@ -19,7 +16,7 @@ window.onload = function() {
   if (window.innerHeight > 950) {
         animatePoints(pointsArray);
   }
-  
+
   var sellingPoints = document.getElementsByClassName('selling-points')[0];
   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
---
+++ scripts/utilities.js
@@ -0,0 +1,8 @@
+// Use a loop to go through all elements in the points array.
+// Execute a callback for each element.
+
+function forEach(array, callback) {
+    for (var i=0; i < array.length; i++) {
+        callback(array[i]);
+    }
+}
